2f507453e58878e948b2f5f4bacd1bc76f6b84b1
indranil.swarnakar <indranil.swarnakar@gmail.com>
2023-09-06 14:08:13 +0530

    d

--- envsetup.sh
+++ envsetup.sh
@@ -12,7 +12,7 @@ echo "Env Setup complete"
 source env/bin/activate
 
 echo "Requeriments Install By Pip Start"
-pip3 install -r backend/requirements.txt
+pip install -r backend/requirements.txt
 echo "Requeriments Install By Pip Success"
 
 if [ -d "logs" ] 
