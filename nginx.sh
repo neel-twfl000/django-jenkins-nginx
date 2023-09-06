54ee4283d2e300a5ea76080a72bddaf5db57aa2f
indranil.swarnakar <indranil.swarnakar@gmail.com>
2023-09-06 14:02:57 +0530

    df

--- nginx.sh
+++ nginx.sh
@@ -1,7 +1,7 @@
-sudo cp -rf app.conf /etc/nginx/sites-available/app
+sudo cp -rf app.conf /etc/nginx/sites-available
 chmod 710 /var/lib/jenkins/workspace/djangi-jenkins-nginx
 
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
@@ -10,3 +10,4 @@ sudo systemctl enable nginx
 echo "Nginx has been started"
 
 sudo systemctl status nginx
+echo "Setup Done"
