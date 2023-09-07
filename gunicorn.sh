886e10b8780d8f4f37b47b74cdd09292817ffad4
indranil.swarnakar <indranil.swarnakar@gmail.com>
2023-09-08 01:27:05 +0530

    d

--- gunicorn.sh
+++ gunicorn.sh
@@ -7,25 +7,12 @@ echo "Pip Complete"
 
 python3 backend/manage.py makemigrations
 python3 backend/manage.py migrate
-python3 backend/manage.py collectstatic
 echo "Migrations done"
-# cd /var/lib/jenkins/workspace/djangi-jenkins-nginx
-
 sudo cp -rf gunicorn.socket /etc/systemd/system/
 sudo cp -rf gunicorn.service /etc/systemd/system/
-
-# echo "$USER"
-# echo "$PWD"
-
-
-
 sudo systemctl daemon-reload
-# # echo "Gunicorn has started."
 sudo systemctl enable gunicorn
 sudo systemctl start gunicorn
-# # echo "Gunicorn has been enabled."
-
-# sudo systemctl restart gunicorn
 sudo systemctl status gunicorn
 source env/bin/activate
 cd /var/lib/jenkins/workspace/django-jenkins-nginx/backend/
